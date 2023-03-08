acf4ccadc84fd0f149f416556aa8a54724fe6bee
JiangWeixian <jiangweixian1994@gmail.com>
2023-03-08 13:44:53 +0800

    :construction:  frame demo

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,17 +1,27 @@
 import React from 'react'
 import './App.css'
-import { ThemeProvider, styled } from 'mayumi/theme'
+import { ThemeProvider } from 'mayumi/theme'
 import * as AspectRatio from '@radix-ui/react-aspect-ratio'
 import { Menu } from 'mayumi/menu'
 import { Box } from 'mayumi/box'
+import { Text } from 'mayumi/text'
 
 const FrameDemo = () => (
   <div className="Container">
-    <AspectRatio.Root ratio={16 / 9}>
-      <Box>
+    <AspectRatio.Root ratio={16 / 9} style={{ border: '1px solid red' }}>
+      {/* non-transparent box will make menu hover style wired */}
+      <Box transparent={true}>
         <Menu size="md" ghost={true}>
           <Menu.SubMenu title="Actions">
-            <Menu.Item itemKey="button">Button</Menu.Item>
+            <Menu.Item itemKey="button">
+              <i
+                className="gg-search"
+                style={{ '--ggs': 0.5, color: 'white', fontWeight: 'bold' }}
+              />
+              <Text size="sm" css={{ ml: '$2' }}>
+                Button
+              </Text>
+            </Menu.Item>
             <Menu.Item itemKey="input">Input</Menu.Item>
             <Menu.Item itemKey="tooltip">Tooltip</Menu.Item>
             <Menu.Item itemKey="dropdown">Dropdown</Menu.Item>
