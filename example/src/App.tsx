723ef264fff0786338dda3e55d77961b02e19940
JiangWeixian <jiangweixian1994@gmail.com>
2023-03-08 22:13:41 +0800

    :sparkles:  setup tailwindcss

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -3,37 +3,48 @@ import './App.css'
 import { ThemeProvider } from 'mayumi/theme'
 import * as AspectRatio from '@radix-ui/react-aspect-ratio'
 import { Menu } from 'mayumi/menu'
-import { Box } from 'mayumi/box'
+import { Dot } from 'mayumi/dot'
 import { Text } from 'mayumi/text'
+import { Layout } from 'mayumi/layout'
 
 const FrameDemo = () => (
-  <div className="Container">
-    <AspectRatio.Root ratio={16 / 9} style={{ border: '1px solid red' }}>
+  <div className="Container rounded-xl overflow-hidden shadow-md bg-slate-900">
+    <AspectRatio.Root ratio={16 / 9}>
+      <div className="w-ful h-9 flex items-center border-dark border-b bg-[#363636]">
+        <div className="flex gap-2 px-3">
+          {/* TODO: size: 10px */}
+          <Dot type="danger" />
+          <Dot type="warning" />
+          <Dot type="success" />
+        </div>
+      </div>
       {/* non-transparent box will make menu hover style wired */}
-      <Box transparent={true}>
-        <Menu size="md" ghost={true}>
-          <Menu.SubMenu title="Actions">
-            <Menu.Item itemKey="button">
-              <i
-                className="gg-search"
-                style={{ '--ggs': 0.5, color: 'white', fontWeight: 'bold' }}
-              />
-              <Text size="sm" css={{ ml: '$2' }}>
-                Button
-              </Text>
-            </Menu.Item>
-            <Menu.Item itemKey="input">Input</Menu.Item>
-            <Menu.Item itemKey="tooltip">Tooltip</Menu.Item>
-            <Menu.Item itemKey="dropdown">Dropdown</Menu.Item>
-          </Menu.SubMenu>
-          <Menu.SubMenu title="Display">
-            <Menu.Item itemKey="avatar">Avatar</Menu.Item>
-            <Menu.Item itemKey="box">Box</Menu.Item>
-            <Menu.Item itemKey="description">Description</Menu.Item>
-            <Menu.Item itemKey="separator">Separator</Menu.Item>
-          </Menu.SubMenu>
-        </Menu>
-      </Box>
+      <Layout>
+        <Layout.Aside open={true}>
+          <Menu size="md" ghost={true} css={{ minWidth: '150px' }}>
+            <Menu.SubMenu title="Actions">
+              <Menu.Item itemKey="button">
+                <i
+                  className="gg-search"
+                  style={{ '--ggs': 0.5, color: 'white', fontWeight: 'bold' }}
+                />
+                <Text size="sm" css={{ ml: '$2' }}>
+                  Button
+                </Text>
+              </Menu.Item>
+              <Menu.Item itemKey="input">Input</Menu.Item>
+              <Menu.Item itemKey="tooltip">Tooltip</Menu.Item>
+              <Menu.Item itemKey="dropdown">Dropdown</Menu.Item>
+            </Menu.SubMenu>
+            <Menu.SubMenu title="Display">
+              <Menu.Item itemKey="avatar">Avatar</Menu.Item>
+              <Menu.Item itemKey="box">Box</Menu.Item>
+              <Menu.Item itemKey="description">Description</Menu.Item>
+              <Menu.Item itemKey="separator">Separator</Menu.Item>
+            </Menu.SubMenu>
+          </Menu>
+        </Layout.Aside>
+      </Layout>
     </AspectRatio.Root>
   </div>
 )
